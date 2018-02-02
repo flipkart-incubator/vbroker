77dc6e4bc3cc1c4c3191c3dfc7389d65d1fbedb1
Kaushal Hooda <kaushal.hooda@flipkart.com>
2018-02-02 13:25:18 +0530

    Created TopicMetadataService

---
+++ src/main/java/com/flipkart/vbroker/services/TopicMetadataService.java
@@ -0,0 +1,14 @@
+package com.flipkart.vbroker.services;
+
+/**
+ * Created by hooda on 2/2/18
+ */
+
+public class TopicMetadataService {
+    private final TopicService topicService;
+
+
+    public TopicMetadataService(TopicService topicService) {
+        this.topicService = topicService;
+    }
+}
