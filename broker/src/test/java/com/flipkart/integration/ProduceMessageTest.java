054e4f74aaef4eba2024f1d10ddfb602a2d8ac6b
Vamsi Subhash Achanta <vamsi360@gmail.com>
2018-02-15 15:30:39 +0530

    Added watch methods and test for getChildren() curator WatchEvent - working and documented

--- broker/src/test/java/com/flipkart/integration/ProduceMessageTest.java
+++ broker/src/test/java/com/flipkart/integration/ProduceMessageTest.java
@@ -3,13 +3,13 @@ package com.flipkart.integration;
 import com.flipkart.vbroker.VBrokerConfig;
 import com.flipkart.vbroker.client.VBrokerClient;
 import com.flipkart.vbroker.server.VBrokerServer;
-import com.flipkart.vbroker.services.CuratorService;
 import com.xebialabs.restito.semantics.Condition;
 import com.xebialabs.restito.server.StubServer;
 import org.glassfish.grizzly.http.Method;
 import org.glassfish.grizzly.http.util.HttpStatus;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -26,39 +26,36 @@ import static com.xebialabs.restito.semantics.Condition.uri;
 public class ProduceMessageTest {
     public static final int MOCK_CONSUMER_PORT = 15000;
     VBrokerServer server;
-    StubServer consumer;
+    StubServer stubServer;
 
-    @BeforeSuite
-    public void startServer() throws IOException {
+    @BeforeClass
+    public void setUp() throws IOException {
         VBrokerConfig config = VBrokerConfig.newConfig("broker.properties");
+        server = new VBrokerServer();
+        server.startAsync().awaitRunning();
+        stubServer = new StubServer(MOCK_CONSUMER_PORT).run();
+    }
 
-        Thread t = new Thread(new Runnable() {
-            public void run() {
-                CuratorService curatorService = new CuratorService(null);
-                server = new VBrokerServer(config, curatorService);
-                server.run();
-            }
-        });
-
-        t.start();
-        consumer = new StubServer(MOCK_CONSUMER_PORT).run();
-
+    @AfterClass
+    public void tearDown() {
+        stubServer.stop();
+        server.stopAsync().awaitTerminated();
     }
 
     @Test
     public void shouldProduceAndConsumeMessage() throws IOException, InterruptedException {
-        whenHttp(consumer).match(Condition.post("/messages")).then(status(HttpStatus.OK_200));
+        whenHttp(stubServer).match(Condition.post("/messages")).then(status(HttpStatus.OK_200));
 
         //Send produce request
         VBrokerClient.main(new String[0]);
 
         //Verify the message is consumed
-        verifyHttp(consumer).once(method(Method.POST),
+        verifyHttp(stubServer).once(method(Method.POST),
             uri("/messages"));
     }
 
     @AfterSuite
     public void stopServer() {
-        consumer.stop();
+        stubServer.stop();
     }
 }
