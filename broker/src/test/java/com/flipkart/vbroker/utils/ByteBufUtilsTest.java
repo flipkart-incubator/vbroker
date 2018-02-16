b9b13ec10383eeee7ae1c5d53d4ff1a4d056af42
Govind Ajith <govind.ajith@PW-C02L51ZDDR53.local>
2018-02-16 12:48:01 +0530

    ByteBufUtils added with method to get bytes out of ByteBuffer

---
+++ broker/src/test/java/com/flipkart/vbroker/utils/ByteBufUtilsTest.java
@@ -0,0 +1,37 @@
+package com.flipkart.vbroker.utils;
+
+import com.flipkart.vbroker.entities.Topic;
+import com.flipkart.vbroker.subscribers.DummyEntities;
+import org.testng.annotations.Test;
+
+import java.nio.ByteBuffer;
+import java.util.stream.IntStream;
+
+import static org.testng.Assert.assertEquals;
+
+public class ByteBufUtilsTest {
+
+    @Test
+    public void shouldReturnBytes() {
+
+        byte[] array = {1, 2, 3};
+        ByteBuffer buffer = ByteBuffer.wrap(array);
+        byte[] bytes = ByteBufUtils.getBytes(buffer);
+        IntStream.range(0, array.length).forEach(i -> {
+            assertEquals(array[i], bytes[i]);
+        });
+    }
+
+    @Test
+    public void shouldConvertRightlyForTopicEntity() {
+
+        Topic topic1 = DummyEntities.topic1;
+        byte[] bytes = ByteBufUtils.getBytes(topic1.getByteBuffer());
+
+        ByteBuffer buffer = ByteBuffer.wrap(bytes);
+        Topic topic2 = Topic.getRootAsTopic(buffer);
+        assertEquals(topic1.topicName(), topic2.topicName());
+        assertEquals(topic1.topicId(), topic2.topicId());
+
+    }
+}
