5306b8ced73a3d5a129dad70138b06f45a46096a
Kaushal Hooda <kaushal.hooda@flipkart.com>
2018-02-21 21:46:27 +0530

    Added getlagsresponsehandler, empty for now

---
+++ broker/src/main/java/com/flipkart/vbroker/handlers/GetLagsResponseHandler.java
@@ -0,0 +1,13 @@
+package com.flipkart.vbroker.handlers;
+
+import com.flipkart.vbroker.entities.VResponse;
+
+/**
+ * Created by kaushal.hooda on 21/02/18.
+ */
+public class GetLagsResponseHandler implements ResponseHandler {
+    @Override
+    public void handle(VResponse vResponse) {
+        //TODO What?
+    }
+}
