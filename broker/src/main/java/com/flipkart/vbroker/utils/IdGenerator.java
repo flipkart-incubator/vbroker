c6e68ad5b12aef3f0bc576a96aee07ba4bcb11bb
Govind Ajith <govind.ajith@PW-C02L51ZDDR53.local>
2018-02-21 14:02:09 +0530

    id generator

---
+++ broker/src/main/java/com/flipkart/vbroker/utils/IdGenerator.java
@@ -0,0 +1,12 @@
+package com.flipkart.vbroker.utils;
+
+import java.util.Random;
+
+public class IdGenerator {
+
+	private static Random random = new Random();
+
+	public static short randomId() {
+		return (short) random.nextInt(Short.MAX_VALUE + 1);
+	}
+}
