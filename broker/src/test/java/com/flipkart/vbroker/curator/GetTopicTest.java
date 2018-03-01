c6cadaf76a5e2d0914ef1123a227ded1ade0f812
Govind Ajith <govind.ajith@PW-C02L51ZDDR53.local>
2018-03-01 12:32:05 +0530

    local test changes

--- broker/src/test/java/com/flipkart/vbroker/curator/GetTopicTest.java
+++ broker/src/test/java/com/flipkart/vbroker/curator/GetTopicTest.java
@@ -1,62 +1,57 @@
 package com.flipkart.vbroker.curator;
 
 import com.flipkart.vbroker.VBrokerConfig;
-import com.flipkart.vbroker.entities.Subscription;
 import com.flipkart.vbroker.entities.Topic;
 import com.flipkart.vbroker.services.CuratorService;
 import com.flipkart.vbroker.subscribers.DummyEntities;
 import com.flipkart.vbroker.utils.ByteBufUtils;
-import com.flipkart.vbroker.utils.SubscriptionUtils;
-import com.flipkart.vbroker.utils.TopicUtils;
-
-import java.nio.ByteBuffer;
-
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.curator.x.async.AsyncCuratorFramework;
 import org.apache.zookeeper.CreateMode;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+
+import java.nio.ByteBuffer;
 
 public class GetTopicTest {
 
-	VBrokerConfig config;
-	CuratorService curatorService;
+    VBrokerConfig config;
+    CuratorService curatorService;
 
-	@BeforeClass
-	public void init() throws Exception {
-		config = VBrokerConfig.newConfig("broker.properties");
-		CuratorFramework curatorClient = CuratorFrameworkFactory.newClient(config.getZookeeperUrl(),
-				new ExponentialBackoffRetry(1000, 5));
-		curatorClient.start();
-		AsyncCuratorFramework asyncZkClient = AsyncCuratorFramework.wrap(curatorClient);
+    @BeforeClass
+    public void init() throws Exception {
+        config = VBrokerConfig.newConfig("broker.properties");
+        CuratorFramework curatorClient = CuratorFrameworkFactory.newClient(config.getZookeeperUrl(),
+            new ExponentialBackoffRetry(1000, 5));
+        curatorClient.start();
+        AsyncCuratorFramework asyncZkClient = AsyncCuratorFramework.wrap(curatorClient);
 
-		curatorService = new CuratorService(asyncZkClient);
-	}
+        curatorService = new CuratorService(asyncZkClient);
+    }
 
-	//@Test
-	public void createTopicTest() throws Exception {
-		String topicPath = config.getTopicsPath() + "/" + "101";
-		Topic topic = DummyEntities.groupedTopic;
-		curatorService.createNodeAndSetData(topicPath, CreateMode.PERSISTENT,
-				ByteBufUtils.getBytes(topic.getByteBuffer())).handleAsync((data, exception) -> {
-					System.out.println("created " + data);
-					return null;
-				}).toCompletableFuture().get();
-	}
+    //@Test
+    public void createTopicTest() throws Exception {
+        String topicPath = config.getTopicsPath() + "/" + "101";
+        Topic topic = DummyEntities.groupedTopic;
+        curatorService.createNodeAndSetData(topicPath, CreateMode.PERSISTENT,
+            ByteBufUtils.getBytes(topic.getByteBuffer())).handleAsync((data, exception) -> {
+            System.out.println("created " + data);
+            return null;
+        }).toCompletableFuture().get();
+    }
 
-	//@Test
-	public void testGet() throws Exception {
-		
-		curatorService.getData("/topics/101").handleAsync((data, exception) -> {
-			Topic t = 	Topic.getRootAsTopic(ByteBuffer.wrap(data));
-			System.out.println(t.name());
-			System.out.println(t.partitions());
-			System.out.println(t.topicCategory());
-			return null;
-		}).toCompletableFuture().join();
-		
+    //@Test
+    public void testGet() throws Exception {
+
+        curatorService.getData("/topics/101").handleAsync((data, exception) -> {
+            Topic t = Topic.getRootAsTopic(ByteBuffer.wrap(data));
+            System.out.println(t.name());
+            System.out.println(t.partitions());
+            System.out.println(t.topicCategory());
+            return null;
+        }).toCompletableFuture().join();
+
 //		curatorService.getData("/admin/create_subscription/31989").handleAsync((data, exception) -> {
 //			Subscription s = SubscriptionUtils.getSubscription(data);
 //			System.out.println(s.topicId());
@@ -65,5 +60,5 @@ public class GetTopicTest {
 //			return null;
 //		}).toCompletableFuture().join();
 
-	}
+    }
 }
--- broker/src/test/java/com/flipkart/vbroker/curator/TopicTest.java
+++ broker/src/test/java/com/flipkart/vbroker/curator/TopicTest.java
@@ -1,7 +1,13 @@
 package com.flipkart.vbroker.curator;
 
-import java.nio.ByteBuffer;
-
+import com.flipkart.vbroker.VBrokerConfig;
+import com.flipkart.vbroker.data.InMemorySubPartDataManager;
+import com.flipkart.vbroker.data.SubPartDataManager;
+import com.flipkart.vbroker.data.TopicPartDataManager;
+import com.flipkart.vbroker.data.memory.InMemoryTopicPartDataManager;
+import com.flipkart.vbroker.entities.Subscription;
+import com.flipkart.vbroker.entities.Topic;
+import com.flipkart.vbroker.services.*;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
@@ -9,75 +15,81 @@ import org.apache.curator.x.async.AsyncCuratorFramework;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import com.flipkart.vbroker.VBrokerConfig;
-import com.flipkart.vbroker.data.InMemorySubPartDataManager;
-import com.flipkart.vbroker.data.SubPartDataManager;
-import com.flipkart.vbroker.data.TopicPartDataManager;
-import com.flipkart.vbroker.data.memory.InMemoryTopicPartDataManager;
-import com.flipkart.vbroker.entities.Topic;
-import com.flipkart.vbroker.services.CuratorService;
-import com.flipkart.vbroker.services.SubscriptionService;
-import com.flipkart.vbroker.services.SubscriptionServiceImpl;
-import com.flipkart.vbroker.services.TopicService;
-import com.flipkart.vbroker.services.TopicServiceImpl;
-import com.flipkart.vbroker.subscribers.DummyEntities;
-import net.bytebuddy.ByteBuddy;
-
 public class TopicTest {
-	VBrokerConfig config;
-	CuratorService curatorService;
-	TopicService topicService;
-	SubscriptionService subscriptionService;
+    VBrokerConfig config;
+    CuratorService curatorService;
+    TopicService topicService;
+    SubscriptionService subscriptionService;
 
-	@BeforeClass
-	public void init() throws Exception {
-		config = VBrokerConfig.newConfig("broker.properties");
-		CuratorFramework curatorClient = CuratorFrameworkFactory.newClient(config.getZookeeperUrl(),
-				new ExponentialBackoffRetry(1000, 5));
-		curatorClient.start();
-		AsyncCuratorFramework asyncZkClient = AsyncCuratorFramework.wrap(curatorClient);
+    @BeforeClass
+    public void init() throws Exception {
+        config = VBrokerConfig.newConfig("broker.properties");
+        CuratorFramework curatorClient = CuratorFrameworkFactory.newClient(config.getZookeeperUrl(),
+            new ExponentialBackoffRetry(1000, 5));
+        curatorClient.start();
+        AsyncCuratorFramework asyncZkClient = AsyncCuratorFramework.wrap(curatorClient);
 
-		curatorService = new CuratorService(asyncZkClient);
-		topicService = new TopicServiceImpl(config, curatorService);
-		TopicPartDataManager topicPartDataManager = new InMemoryTopicPartDataManager();
-		SubPartDataManager subPartDataManager = new InMemorySubPartDataManager(topicPartDataManager);
-		subscriptionService = new SubscriptionServiceImpl(config, curatorService, topicPartDataManager, subPartDataManager, topicService);
-	}
+        curatorService = new CuratorService(asyncZkClient);
+        topicService = new TopicServiceImpl(config, curatorService);
+        TopicPartDataManager topicPartDataManager = new InMemoryTopicPartDataManager();
+        SubPartDataManager subPartDataManager = new InMemorySubPartDataManager(topicPartDataManager);
+        subscriptionService = new SubscriptionServiceImpl(config, curatorService, topicPartDataManager, subPartDataManager, topicService);
+    }
 
-	//@Test
-	public void test() throws Exception {
-		// Topic topic = DummyEntities.groupedTopic;
-		// topicService.createTopic(topic).handleAsync((data, exception) -> {
-		// System.out.println("created topic");
-		// return null;
-		// }).toCompletableFuture().get();
+    @Test
+    public void test() throws Exception {
+        // Topic topic = DummyEntities.groupedTopic;
+        // topicService.createTopic(topic).handleAsync((data, exception) -> {
+        // System.out.println("created topic");
+        // return null;
+        // }).toCompletableFuture().get();
 
-		// subscriptionService.createSubscriptionAdmin((short) 201,
-		// DummyEntities.groupedSubscription)
-		// .handleAsync((data, exception) -> {
-		// System.out.println("created sub");
-		// return null;
-		// });
+        // subscriptionService.createSubscriptionAdmin((short) 201,
+        // DummyEntities.groupedSubscription)
+        // .handleAsync((data, exception) -> {
+        // System.out.println("created sub");
+        // return null;
+        // });
 
-		subscriptionService.getSubscription((short) 5619, (short) 15929).handleAsync((data, exc) -> {
-			System.out.println(data.name());
-			System.out.println(data.httpUri());
-			return null;
-		}).toCompletableFuture().get();
-		
-		subscriptionService.getSubscriptionsForTopic((short)5619).handleAsync((data, exc) -> {
-			System.out.println(data.get(0).name());
-			System.out.println(data.get(0).httpUri());
-			return null;
-		}).toCompletableFuture().get();
+        topicService.getAllTopics().handleAsync((data, exc) -> {
+            for (Topic t : data) {
+                System.out.println(t.id());
+                System.out.println(t.name());
+            }
+            System.out.println(exc);
+            return null;
+        }).toCompletableFuture().get();
 
-		// topicService.getTopic((short) 101).handleAsync((dat, exc) -> {
-		// System.out.println(dat.name());
-		// System.out.println(dat.grouped());
-		// System.out.println(dat.replicationFactor());
-		// return null;
-		// });
+        subscriptionService.getAllSubscriptions().handleAsync((data, exc) -> {
+            for (Subscription s : data) {
+                System.out.println(s.id());
+                System.out.println(s.name());
+                System.out.println(s.httpMethod());
+            }
+            System.out.println(exc);
+            return null;
+        }).toCompletableFuture().get();
+        // subscriptionService.getSubscription((short) 27645, (short)
+        // 10502).handleAsync((data, exc) -> {
+        // System.out.println(data.name());
+        // System.out.println(data.httpUri());
+        // return null;
+        // }).toCompletableFuture().get();
+        //
+        // subscriptionService.getSubscriptionsForTopic((short)27645).handleAsync((data,
+        // exc) -> {
+        // System.out.println(data.get(0).name());
+        // System.out.println(data.get(0).httpUri());
+        // return null;
+        // }).toCompletableFuture().get();
 
-	}
+        // topicService.getTopic((short) 101).handleAsync((dat, exc) -> {
+        // System.out.println(dat.name());
+        // System.out.println(dat.grouped());
+        // System.out.println(dat.replicationFactor());
+        // return null;
+        // });
+
+    }
 
 }
