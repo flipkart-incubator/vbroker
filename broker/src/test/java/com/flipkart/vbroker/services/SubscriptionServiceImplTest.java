0277e705739c858728ed58d1b07ef91a936a263e
Govind Ajith <govind.ajith@PW-C02L51ZDDR53.local>
2018-03-09 10:54:47 +0530

    reformatting + new test case for isPresent

--- broker/src/test/java/com/flipkart/vbroker/services/SubscriptionServiceImplTest.java
+++ broker/src/test/java/com/flipkart/vbroker/services/SubscriptionServiceImplTest.java
@@ -27,162 +27,162 @@ import static org.testng.Assert.assertNotNull;
 
 public class SubscriptionServiceImplTest {
 
-	SubscriptionService subscriptionService;
-	@Mock
-	TopicService topicService;
-	@Mock
-	CuratorService curatorService;
-	@Mock
-	TopicPartDataManager topicPartDataManager;
-	@Mock
-	SubPartDataManager subPartDataManager;
-	VBrokerConfig config;
+    SubscriptionService subscriptionService;
+    @Mock
+    TopicService topicService;
+    @Mock
+    CuratorService curatorService;
+    @Mock
+    TopicPartDataManager topicPartDataManager;
+    @Mock
+    SubPartDataManager subPartDataManager;
+    VBrokerConfig config;
 
-	@BeforeClass
-	public void init() throws Exception {
+    @BeforeClass
+    public void init() throws Exception {
 
-		MockitoAnnotations.initMocks(this);
-		config = VBrokerConfig.newConfig("broker.properties");
-		subscriptionService = new SubscriptionServiceImpl(config, curatorService, topicPartDataManager,
-				subPartDataManager, topicService);
-	}
+        MockitoAnnotations.initMocks(this);
+        config = VBrokerConfig.newConfig("broker.properties");
+        subscriptionService = new SubscriptionServiceImpl(config, curatorService, topicPartDataManager,
+            subPartDataManager, topicService);
+    }
 
-	@Test
-	public void shouldCreateSubscription() throws Exception {
-		Subscription subscription = DummyEntities.groupedSubscription;
-		CompletionStage<String> value = CompletableFuture.supplyAsync(() -> {
-			return "subscriptions/0001";
-		});
-		when(curatorService.createNodeAndSetData(any(String.class), any(CreateMode.class), any(), any()))
-				.thenReturn(value);
-		subscriptionService.createSubscription(subscription).handle((data, exception) -> {
-			assertEquals(data.elastic(), subscription.elastic());
-			assertEquals(data.httpMethod(), subscription.httpMethod());
-			return null;
-		}).toCompletableFuture().get();
-	}
+    @Test
+    public void shouldCreateSubscription() throws Exception {
+        Subscription subscription = DummyEntities.groupedSubscription;
+        CompletionStage<String> value = CompletableFuture.supplyAsync(() -> {
+            return "subscriptions/0001";
+        });
+        when(curatorService.createNodeAndSetData(any(String.class), any(CreateMode.class), any(), any()))
+            .thenReturn(value);
+        subscriptionService.createSubscription(subscription).handle((data, exception) -> {
+            assertEquals(data.elastic(), subscription.elastic());
+            assertEquals(data.httpMethod(), subscription.httpMethod());
+            return null;
+        }).toCompletableFuture().get();
+    }
 
-	@Test
-	public void shouldGetAllSubscriptions() throws Exception {
-		CompletionStage<List<Topic>> value = CompletableFuture.supplyAsync(() -> {
-			List<Topic> topics = new ArrayList<Topic>();
-			topics.add(DummyEntities.groupedTopic);
-			return topics;
-		});
-		when(topicService.getAllTopics()).thenReturn(value);
+    @Test
+    public void shouldGetAllSubscriptions() throws Exception {
+        CompletionStage<List<Topic>> value = CompletableFuture.supplyAsync(() -> {
+            List<Topic> topics = new ArrayList<Topic>();
+            topics.add(DummyEntities.groupedTopic);
+            return topics;
+        });
+        when(topicService.getAllTopics()).thenReturn(value);
 
-		String subsPath = config.getTopicsPath() + "/" + DummyEntities.groupedTopic.id() + "/subscriptions";
-		CompletionStage<List<String>> subscriptionIds = CompletableFuture.supplyAsync(() -> {
-			List<String> subIds = new ArrayList<>();
-			subIds.add(String.valueOf(DummyEntities.groupedSubscription.id()));
-			return subIds;
-		});
-		when(curatorService.getChildren(subsPath)).thenReturn(subscriptionIds);
+        String subsPath = config.getTopicsPath() + "/" + DummyEntities.groupedTopic.id() + "/subscriptions";
+        CompletionStage<List<String>> subscriptionIds = CompletableFuture.supplyAsync(() -> {
+            List<String> subIds = new ArrayList<>();
+            subIds.add(String.valueOf(DummyEntities.groupedSubscription.id()));
+            return subIds;
+        });
+        when(curatorService.getChildren(subsPath)).thenReturn(subscriptionIds);
 
-		String subPath = config.getTopicsPath() + "/" + DummyEntities.groupedTopic.id() + "/subscriptions/"
-				+ DummyEntities.groupedSubscription.id();
-		CompletionStage<byte[]> subscription = CompletableFuture.supplyAsync(() -> {
-			return DummyEntities.groupedSubscription.toBytes();
-		});
-		when(curatorService.getData(subPath)).thenReturn(subscription);
+        String subPath = config.getTopicsPath() + "/" + DummyEntities.groupedTopic.id() + "/subscriptions/"
+            + DummyEntities.groupedSubscription.id();
+        CompletionStage<byte[]> subscription = CompletableFuture.supplyAsync(() -> {
+            return DummyEntities.groupedSubscription.toBytes();
+        });
+        when(curatorService.getData(subPath)).thenReturn(subscription);
 
-		subscriptionService.getAllSubscriptions().handle((data, exception) -> {
-			assertEquals(data.get(0).name(), DummyEntities.groupedSubscription.name());
-			return null;
-		}).toCompletableFuture().get();
-	}
+        subscriptionService.getAllSubscriptions().handle((data, exception) -> {
+            assertEquals(data.get(0).name(), DummyEntities.groupedSubscription.name());
+            return null;
+        }).toCompletableFuture().get();
+    }
 
-	@Test
-	public void shouldGetSubscription() throws Exception {
-		String subscriptionPath = config.getTopicsPath() + "/" + DummyEntities.groupedTopic.id() + "/subscriptions/"
-				+ DummyEntities.groupedSubscription.id();
-		CompletionStage<byte[]> subValue = CompletableFuture.supplyAsync(() -> {
-			return DummyEntities.groupedSubscription.toBytes();
-		});
-		when(curatorService.getData(subscriptionPath)).thenReturn(subValue);
-		subscriptionService.getSubscription(DummyEntities.groupedTopic.id(), DummyEntities.groupedSubscription.id())
-				.handle((data, exception) -> {
-					assertEquals(data.name(), DummyEntities.groupedSubscription.name());
-					return null;
-				}).toCompletableFuture().get();
+    @Test
+    public void shouldGetSubscription() throws Exception {
+        String subscriptionPath = config.getTopicsPath() + "/" + DummyEntities.groupedTopic.id() + "/subscriptions/"
+            + DummyEntities.groupedSubscription.id();
+        CompletionStage<byte[]> subValue = CompletableFuture.supplyAsync(() -> {
+            return DummyEntities.groupedSubscription.toBytes();
+        });
+        when(curatorService.getData(subscriptionPath)).thenReturn(subValue);
+        subscriptionService.getSubscription(DummyEntities.groupedTopic.id(), DummyEntities.groupedSubscription.id())
+            .handle((data, exception) -> {
+                assertEquals(data.name(), DummyEntities.groupedSubscription.name());
+                return null;
+            }).toCompletableFuture().get();
 
-	}
+    }
 
-	@Test
-	public void shouldGetSubscriptionsForTopic() throws Exception {
-		int topicId = DummyEntities.groupedTopic.id();
-		int subId = DummyEntities.groupedSubscription.id();
-		String subsPath = config.getTopicsPath() + "/" + topicId + "/subscriptions";
-		CompletionStage<List<String>> subIds = CompletableFuture.supplyAsync(() -> {
-			List<String> ids = new ArrayList<>();
-			ids.add(String.valueOf(subId));
-			return ids;
-		});
-		when(curatorService.getChildren(subsPath)).thenReturn(subIds);
-		subscriptionService.getSubscriptionsForTopic(topicId).handle((data, exception) -> {
-			assertEquals(data.size(), 1);
-			assertEquals(data.get(0).name(), DummyEntities.groupedSubscription.name());
-			return null;
-		}).toCompletableFuture().get();
-	}
+    @Test
+    public void shouldGetSubscriptionsForTopic() throws Exception {
+        int topicId = DummyEntities.groupedTopic.id();
+        int subId = DummyEntities.groupedSubscription.id();
+        String subsPath = config.getTopicsPath() + "/" + topicId + "/subscriptions";
+        CompletionStage<List<String>> subIds = CompletableFuture.supplyAsync(() -> {
+            List<String> ids = new ArrayList<>();
+            ids.add(String.valueOf(subId));
+            return ids;
+        });
+        when(curatorService.getChildren(subsPath)).thenReturn(subIds);
+        subscriptionService.getSubscriptionsForTopic(topicId).handle((data, exception) -> {
+            assertEquals(data.size(), 1);
+            assertEquals(data.get(0).name(), DummyEntities.groupedSubscription.name());
+            return null;
+        }).toCompletableFuture().get();
+    }
 
-	@Test
-	public void shouldGetPartSubscription() throws Exception {
-		Subscription subscription = DummyEntities.groupedSubscription;
-		int partSubscriptionId = 0;
+    @Test
+    public void shouldGetPartSubscription() throws Exception {
+        Subscription subscription = DummyEntities.groupedSubscription;
+        int partSubscriptionId = 0;
 
-		String subscriptionPath = config.getTopicsPath() + "/" + DummyEntities.groupedTopic.id() + "/subscriptions/"
-				+ DummyEntities.groupedSubscription.id();
-		CompletionStage<byte[]> subValue = CompletableFuture.supplyAsync(() -> {
-			return DummyEntities.groupedSubscription.toBytes();
-		});
-		when(curatorService.getData(subscriptionPath)).thenReturn(subValue);
+        String subscriptionPath = config.getTopicsPath() + "/" + DummyEntities.groupedTopic.id() + "/subscriptions/"
+            + DummyEntities.groupedSubscription.id();
+        CompletionStage<byte[]> subValue = CompletableFuture.supplyAsync(() -> {
+            return DummyEntities.groupedSubscription.toBytes();
+        });
+        when(curatorService.getData(subscriptionPath)).thenReturn(subValue);
 
-		subscriptionService.getPartSubscription(subscription, partSubscriptionId).handle((data, exception) -> {
-			assertEquals(data.getSubscriptionId(), DummyEntities.groupedSubscription.id());
-			assertEquals(data.getTopicPartition().getTopicId(), DummyEntities.groupedSubscription.topicId());
-			return null;
-		}).toCompletableFuture().get();
-	}
+        subscriptionService.getPartSubscription(subscription, partSubscriptionId).handle((data, exception) -> {
+            assertEquals(data.getSubscriptionId(), DummyEntities.groupedSubscription.id());
+            assertEquals(data.getTopicPartition().getTopicId(), DummyEntities.groupedSubscription.topicId());
+            return null;
+        }).toCompletableFuture().get();
+    }
 
-	@Test
-	public void shouldFail_PartSubscription() throws Exception {
-		Subscription subscription = DummyEntities.groupedSubscription;
-		int partSubscriptionId = 0;
+    @Test
+    public void shouldFail_PartSubscription() throws Exception {
+        Subscription subscription = DummyEntities.groupedSubscription;
+        int partSubscriptionId = 0;
 
-		String subscriptionPath = config.getTopicsPath() + "/" + DummyEntities.groupedTopic.id() + "/subscriptions/"
-				+ DummyEntities.groupedSubscription.id();
-		CompletionStage<byte[]> subValue = CompletableFuture.supplyAsync(() -> {
-			throw new VBrokerException("unknown error");
-		});
-		when(curatorService.getData(subscriptionPath)).thenReturn(subValue);
+        String subscriptionPath = config.getTopicsPath() + "/" + DummyEntities.groupedTopic.id() + "/subscriptions/"
+            + DummyEntities.groupedSubscription.id();
+        CompletionStage<byte[]> subValue = CompletableFuture.supplyAsync(() -> {
+            throw new VBrokerException("unknown error");
+        });
+        when(curatorService.getData(subscriptionPath)).thenReturn(subValue);
 
-		subscriptionService.getPartSubscription(subscription, partSubscriptionId).handle((data, exception) -> {
-			assertNotNull(exception);
-			return null;
-		}).toCompletableFuture().get();
-	}
+        subscriptionService.getPartSubscription(subscription, partSubscriptionId).handle((data, exception) -> {
+            assertNotNull(exception);
+            return null;
+        }).toCompletableFuture().get();
+    }
 
-	@Test
-	public void shouldGetPartSubscriber() throws Exception {
-		Subscription subscription = DummyEntities.groupedSubscription;
-		int topicId = DummyEntities.groupedTopic.id();
-		int subscriptionId = DummyEntities.groupedSubscription.id();
-		boolean grouped = true;
+    @Test
+    public void shouldGetPartSubscriber() throws Exception {
+        Subscription subscription = DummyEntities.groupedSubscription;
+        int topicId = DummyEntities.groupedTopic.id();
+        int subscriptionId = DummyEntities.groupedSubscription.id();
+        boolean grouped = true;
 
-		String subscriptionPath = config.getTopicsPath() + "/" + topicId + "/subscriptions/" + subscriptionId;
-		CompletionStage<byte[]> subValue = CompletableFuture.supplyAsync(() -> {
-			return DummyEntities.groupedSubscription.toBytes();
-		});
-		when(curatorService.getData(subscriptionPath)).thenReturn(subValue);
+        String subscriptionPath = config.getTopicsPath() + "/" + topicId + "/subscriptions/" + subscriptionId;
+        CompletionStage<byte[]> subValue = CompletableFuture.supplyAsync(() -> {
+            return DummyEntities.groupedSubscription.toBytes();
+        });
+        when(curatorService.getData(subscriptionPath)).thenReturn(subValue);
 
-		TopicPartition topicPartition = new TopicPartition(0, topicId, grouped);
-		PartSubscription partSubscription = new PartSubscription(0, topicPartition, subscriptionId, grouped);
-		subscriptionService.getPartSubscriber(partSubscription).handle((data, exception) -> {
-			assertEquals(data.getPartSubscription().getSubscriptionId(), subscriptionId);
-			assertEquals(data.getPartSubscription().getId(), 0);
-			return null;
-		}).toCompletableFuture().get();
-	}
+        TopicPartition topicPartition = new TopicPartition(0, topicId, grouped);
+        PartSubscription partSubscription = new PartSubscription(0, topicPartition, subscriptionId, grouped);
+        subscriptionService.getPartSubscriber(partSubscription).handle((data, exception) -> {
+            assertEquals(data.getPartSubscription().getSubscriptionId(), subscriptionId);
+            assertEquals(data.getPartSubscription().getId(), 0);
+            return null;
+        }).toCompletableFuture().get();
+    }
 
 }
--- broker/src/test/java/com/flipkart/vbroker/services/TopicServiceImplTest.java
+++ broker/src/test/java/com/flipkart/vbroker/services/TopicServiceImplTest.java
@@ -18,126 +18,158 @@ import java.util.concurrent.CompletionStage;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.*;
 
 public class TopicServiceImplTest {
 
-	TopicService topicService;
-	@Mock
-	CuratorService curatorService;
-	VBrokerConfig config = null;
+    TopicService topicService;
+    @Mock
+    CuratorService curatorService;
+    VBrokerConfig config = null;
 
-	@BeforeClass
-	public void init() throws Exception {
+    @BeforeClass
+    public void init() throws Exception {
 
-		MockitoAnnotations.initMocks(this);
-		config = VBrokerConfig.newConfig("broker.properties");
-		topicService = new TopicServiceImpl(config, curatorService);
-	}
+        MockitoAnnotations.initMocks(this);
+        config = VBrokerConfig.newConfig("broker.properties");
+        topicService = new TopicServiceImpl(config, curatorService);
+    }
 
-	@Test
-	public void shouldCreateTopic() throws Exception {
-		CompletionStage<String> value = CompletableFuture.supplyAsync(() -> {
-			return "topics/0001";
-		});
-		when(curatorService.createNodeAndSetData(any(String.class), any(CreateMode.class), any(), any()))
-				.thenReturn(value);
-		Topic topic1 = DummyEntities.groupedTopic;
-		topicService.createTopic(topic1).handle((data, exception) -> {
-			// assertEquals(data.id(), 0001);
-			assertEquals(topic1.name(), data.name());
-			assertEquals(topic1.grouped(), data.grouped());
-			return null;
-		}).toCompletableFuture().get();
-	}
+    @Test
+    public void shouldCreateTopic() throws Exception {
+        CompletionStage<String> value = CompletableFuture.supplyAsync(() -> {
+            return "topics/0001";
+        });
+        when(curatorService.createNodeAndSetData(any(String.class), any(CreateMode.class), any(), any()))
+            .thenReturn(value);
+        Topic topic1 = DummyEntities.groupedTopic;
+        topicService.createTopic(topic1).handle((data, exception) -> {
+            // assertEquals(data.id(), 0001);
+            assertEquals(topic1.name(), data.name());
+            assertEquals(topic1.grouped(), data.grouped());
+            return null;
+        }).toCompletableFuture().get();
+    }
 
-	// @Test
-	public void shouldCreateTopic_AndFail_ForWrongId() throws Exception {
-		CompletionStage<String> value = CompletableFuture.supplyAsync(() -> {
-			return "0001";
-		});
-		when(curatorService.createNodeAndSetData(any(String.class), any(CreateMode.class), any(), any()))
-				.thenReturn(value);
-		Topic topic1 = DummyEntities.groupedTopic;
-		topicService.createTopic(topic1).handle((data, exception) -> {
-			assertTrue(exception.getCause() instanceof ArrayIndexOutOfBoundsException);
-			return null;
-		}).toCompletableFuture().get();
-	}
+    // @Test
+    public void shouldCreateTopic_AndFail_ForWrongId() throws Exception {
+        CompletionStage<String> value = CompletableFuture.supplyAsync(() -> {
+            return "0001";
+        });
+        when(curatorService.createNodeAndSetData(any(String.class), any(CreateMode.class), any(), any()))
+            .thenReturn(value);
+        Topic topic1 = DummyEntities.groupedTopic;
+        topicService.createTopic(topic1).handle((data, exception) -> {
+            assertTrue(exception.getCause() instanceof ArrayIndexOutOfBoundsException);
+            return null;
+        }).toCompletableFuture().get();
+    }
 
-	@Test
-	public void shouldFailCreateTopic_ForCuratorException() throws Exception {
-		CompletionStage<String> value = CompletableFuture.supplyAsync(() -> {
-			throw new VBrokerException("Unknown error");
-		});
-		when(curatorService.createNodeAndSetData(any(String.class), any(CreateMode.class), any(), any()))
-				.thenReturn(value);
-		Topic topic1 = DummyEntities.groupedTopic;
-		topicService.createTopic(topic1).handle((data, exception) -> {
-			assertTrue(exception.getCause() instanceof TopicCreationException);
-			return null;
-		}).toCompletableFuture().get();
-	}
+    @Test
+    public void shouldFailCreateTopic_ForCuratorException() throws Exception {
+        CompletionStage<String> value = CompletableFuture.supplyAsync(() -> {
+            throw new VBrokerException("Unknown error");
+        });
+        when(curatorService.createNodeAndSetData(any(String.class), any(CreateMode.class), any(), any()))
+            .thenReturn(value);
+        Topic topic1 = DummyEntities.groupedTopic;
+        topicService.createTopic(topic1).handle((data, exception) -> {
+            assertTrue(exception.getCause() instanceof TopicCreationException);
+            return null;
+        }).toCompletableFuture().get();
+    }
 
-	@Test
-	public void shouldGetTopic() throws Exception {
-		int topicId = DummyEntities.groupedTopic.id();
-		String path = config.getTopicsPath() + "/" + topicId;
-		CompletionStage<byte[]> topicBytes = CompletableFuture.supplyAsync(() -> {
-			return DummyEntities.groupedTopic.toBytes();
-		});
-		when(curatorService.getData(path)).thenReturn(topicBytes);
-		topicService.getTopic(topicId).handle((data, exception) -> {
-			assertEquals(data.topicCategory(), DummyEntities.groupedTopic.topicCategory());
-			assertEquals(data.name(), DummyEntities.groupedTopic.name());
-			return null;
-		});
-	}
+    @Test
+    public void shouldGetTopic() throws Exception {
+        int topicId = DummyEntities.groupedTopic.id();
+        String path = config.getTopicsPath() + "/" + topicId;
+        CompletionStage<byte[]> topicBytes = CompletableFuture.supplyAsync(() -> {
+            return DummyEntities.groupedTopic.toBytes();
+        });
+        when(curatorService.getData(path)).thenReturn(topicBytes);
+        topicService.getTopic(topicId).handle((data, exception) -> {
+            assertEquals(data.topicCategory(), DummyEntities.groupedTopic.topicCategory());
+            assertEquals(data.name(), DummyEntities.groupedTopic.name());
+            return null;
+        }).toCompletableFuture().get();
+    }
 
-	@Test
-	public void shouldGetAllTopics() throws Exception {
-		int topicId1 = DummyEntities.groupedTopic.id();
-		int topicId2 = DummyEntities.unGroupedTopic.id();
-		CompletionStage<List<String>> topicIds = CompletableFuture.supplyAsync(() -> {
-			List<String> ids = new ArrayList();
-			ids.add(String.valueOf(topicId1));
-			ids.add(String.valueOf(topicId2));
-			return ids;
-		});
-		
-		CompletionStage<byte[]> topic1Bytes = CompletableFuture.supplyAsync(() -> {
-			return DummyEntities.groupedTopic.toBytes();
-		});
-		CompletionStage<byte[]> topic2Bytes = CompletableFuture.supplyAsync(() -> {
-			return DummyEntities.unGroupedTopic.toBytes();
-		});
-		when(curatorService.getData(config.getTopicsPath() + "/" + topicId1)).thenReturn(topic1Bytes);
-		when(curatorService.getData(config.getTopicsPath() + "/" + topicId2)).thenReturn(topic2Bytes);
-		
-		when(curatorService.getChildren(config.getTopicsPath())).thenReturn(topicIds);
-		topicService.getAllTopics().handle((data, exception) -> {
-			assertEquals(data.size(), 2);
-			return null;
-		});
-	}
+    @Test
+    public void shouldGetAllTopics() throws Exception {
+        int topicId1 = DummyEntities.groupedTopic.id();
+        int topicId2 = DummyEntities.unGroupedTopic.id();
+        CompletionStage<List<String>> topicIds = CompletableFuture.supplyAsync(() -> {
+            List<String> ids = new ArrayList();
+            ids.add(String.valueOf(topicId1));
+            ids.add(String.valueOf(topicId2));
+            return ids;
+        });
 
-	@Test
-	public void shouldGetTopicPartition() throws Exception {
-		Topic topic = DummyEntities.groupedTopic;
-		int topicPartitionId = 0;
-		int topicId = DummyEntities.groupedTopic.id();
-		String path = config.getTopicsPath() + "/" + topicId;
-		CompletionStage<byte[]> topicBytes = CompletableFuture.supplyAsync(() -> {
-			return DummyEntities.groupedTopic.toBytes();
-		});
-		when(curatorService.getData(path)).thenReturn(topicBytes);
-		topicService.getTopicPartition(topic, topicPartitionId).handle((data, exception) -> {
-			assertEquals(data.isGrouped(), DummyEntities.groupedTopic.grouped());
-			assertEquals(data.getTopicId(), topicId);
-			assertEquals(data.getId(), 0);
-			return null;
-		});
-	}
+        CompletionStage<byte[]> topic1Bytes = CompletableFuture.supplyAsync(() -> {
+            return DummyEntities.groupedTopic.toBytes();
+        });
+        CompletionStage<byte[]> topic2Bytes = CompletableFuture.supplyAsync(() -> {
+            return DummyEntities.unGroupedTopic.toBytes();
+        });
+        when(curatorService.getData(config.getTopicsPath() + "/" + topicId1)).thenReturn(topic1Bytes);
+        when(curatorService.getData(config.getTopicsPath() + "/" + topicId2)).thenReturn(topic2Bytes);
+
+        when(curatorService.getChildren(config.getTopicsPath())).thenReturn(topicIds);
+        topicService.getAllTopics().handle((data, exception) -> {
+            assertEquals(data.size(), 2);
+            return null;
+        }).toCompletableFuture().get();
+    }
+
+    @Test
+    public void shouldGetTopicPartition() throws Exception {
+        Topic topic = DummyEntities.groupedTopic;
+        int topicPartitionId = 0;
+        int topicId = DummyEntities.groupedTopic.id();
+        String path = config.getTopicsPath() + "/" + topicId;
+        CompletionStage<byte[]> topicBytes = CompletableFuture.supplyAsync(() -> {
+            return DummyEntities.groupedTopic.toBytes();
+        });
+        when(curatorService.getData(path)).thenReturn(topicBytes);
+        topicService.getTopicPartition(topic, topicPartitionId).handle((data, exception) -> {
+            assertEquals(data.isGrouped(), DummyEntities.groupedTopic.grouped());
+            assertEquals(data.getTopicId(), topicId);
+            assertEquals(data.getId(), 0);
+            return null;
+        }).toCompletableFuture().get();
+    }
+
+    @Test
+    public void shouldGetValid_IsTopicPresent() throws Exception {
+        int topicId1 = DummyEntities.groupedTopic.id();
+        int topicId2 = DummyEntities.unGroupedTopic.id();
+        CompletionStage<List<String>> topicIds = CompletableFuture.supplyAsync(() -> {
+            List<String> ids = new ArrayList();
+            ids.add(String.valueOf(topicId1));
+            ids.add(String.valueOf(topicId2));
+            return ids;
+        });
+
+        CompletionStage<byte[]> topic1Bytes = CompletableFuture.supplyAsync(() -> {
+            return DummyEntities.groupedTopic.toBytes();
+        });
+        CompletionStage<byte[]> topic2Bytes = CompletableFuture.supplyAsync(() -> {
+            return DummyEntities.unGroupedTopic.toBytes();
+        });
+        when(curatorService.getData(config.getTopicsPath() + "/" + topicId1)).thenReturn(topic1Bytes);
+        when(curatorService.getData(config.getTopicsPath() + "/" + topicId2)).thenReturn(topic2Bytes);
+
+        when(curatorService.getChildren(config.getTopicsPath())).thenReturn(topicIds);
+
+        topicService.isTopicPresent(DummyEntities.groupedTopic.name()).handle((data, exception) -> {
+            assertTrue(data);
+            return null;
+        }).toCompletableFuture().get();
+
+        topicService.isTopicPresent("non-name").handle((data, exception) -> {
+            assertFalse(data);
+            return null;
+        }).toCompletableFuture().get();
+    }
 
 }
